c25facb3c96e0b510ab59e4bd896d285bcf15fea
Micaela Jenik <micaela.jenik@gmail.com>
2021-07-30 12:58:08 -0300

    added about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount ForestLiana::Engine => '/forest'
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   get 'design', to: 'pages#design'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
