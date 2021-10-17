0b991496d4af922125c0e194a09d86d5fe962ff5
Micaela Jenik <micaela.jenik@gmail.com>
2021-10-17 12:05:36 +0200

    migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'visualizations/index'
-  get 'visualizations/show'
   mount ForestLiana::Engine => '/forest'
   root to: 'pages#welcome'
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'design', to: 'pages#design'
+  resources :visualizations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
