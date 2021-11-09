030d2579b2313be3abe2ac3be8dd78d1f4e1a596
Micaela Jenik <micaela.jenik@gmail.com>
2021-11-09 22:49:06 +0100

    added link to article

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
   get 'design', to: 'pages#design'
   resources :visualizations
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
