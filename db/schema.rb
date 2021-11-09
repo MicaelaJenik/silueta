4eea7f0f93f75bad5b7f37af931446f5bb256833
Micaela Jenik <micaela.jenik@gmail.com>
2021-11-09 22:31:23 +0100

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_17_095907) do
+ActiveRecord::Schema.define(version: 2021_11_09_212916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.string "month"
+    t.string "publication"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "visualizations", force: :cascade do |t|
     t.string "title"
